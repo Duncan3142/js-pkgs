7bef8e220251f97554b6553a704acf4cfe252448
Duncan Giles <duncan3142@gmail.com>
2024-05-18 05:47:42 +0100

    module

--- apps/hermes/lib/modfunc.ex
+++ apps/hermes/lib/modfunc.ex
@@ -1,5 +1,24 @@
 defmodule MyMod do
+  @spec func() :: :ok
   def func do
-    IO.puts("Hello from Mod.func")
+    priv()
+    IO.puts("Hello from MyMod.func")
   end
+
+  defp priv do
+    IO.puts("Hello from MyMod.priv")
+  end
+
+  @spec zero?(number()) :: boolean()
+  def zero?(0), do: true
+  def zero?(0.0), do: true
+  def zero?(x) when is_integer(x), do: false
+  def zero?(x) when is_float(x), do: false
 end
+
+MyMod.func()
+IO.puts(MyMod.zero?(0))
+IO.puts(MyMod.zero?(0.0))
+IO.puts(MyMod.zero?(-0.0))
+IO.puts(MyMod.zero?(1))
+IO.puts(MyMod.zero?(1.0))
