10a4afabb30224105c90b9cc7c07f32fd07ab931
Duncan Giles <duncan3142@gmail.com>
2024-06-14 14:55:44 +0100

    bucket delete

--- apps/hermes/lib/otp/bucket.ex
+++ apps/hermes/lib/otp/bucket.ex
@@ -1,18 +1,48 @@
 defmodule OTP.Bucket do
+  @moduledoc """
+  Agent based key-value store.
+  """
+  use Agent
+
+  @doc """
+  Starts a new bucket linked to the current process.
+  """
   def start_link(opts \\ []) do
     Agent.start_link(fn -> %{} end, opts)
   end
 
-  def set(bucket, key, value) do
-    Agent.update(bucket, fn map -> Map.put(map, key, {:just, value}) end)
+  @doc """
+  Starts a new bucket.
+  """
+  def start(opts \\ []) do
+    Agent.start(fn -> %{} end, opts)
   end
 
+  @doc """
+  Stops a bucket.
+  """
+  def stop(bucket) do
+    Agent.stop(bucket)
+  end
+
+  @doc """
+  Sets a value in a bucket by key.
+  """
+  def set(bucket, key, value) do
+    Agent.update(bucket, &Map.put(&1, key, {:just, value}))
+  end
+
+  @doc """
+  Gets a value from a bucket by key.
+  """
   def get(bucket, key) do
-    Agent.get(bucket, fn map ->
-      case Map.get(map, key, :nothing) do
-        :nothing -> :nothing
-        {:just, value} -> {:just, value}
-      end
-    end)
+    Agent.get(bucket, &Map.get(&1, key, :nothing))
+  end
+
+  @doc """
+  Deletes a value from a bucket by key.
+  """
+  def delete(bucket, key) do
+    Agent.get_and_update(bucket, &Map.pop(&1, key))
   end
 end
--- apps/hermes/test/otp/bucket_test.exs
+++ apps/hermes/test/otp/bucket_test.exs
@@ -1,12 +1,27 @@
 defmodule OTP.BucketTest do
   use ExUnit.Case, async: true
 
-  test "set bucket by key" do
-    {:ok, bucket} = OTP.Bucket.start_link()
+  setup do
+    {:ok, bucket} = OTP.Bucket.start_link([])
+    OTP.Bucket.set(bucket, "setup", "init")
+    %{bucket: bucket}
+  end
+
+  test "read empty key", %{bucket: bucket} do
     r = OTP.Bucket.get(bucket, "key")
     assert r == :nothing
+  end
+
+  test "set bucket by key", %{bucket: bucket} do
     OTP.Bucket.set(bucket, "key", "value")
     {:just, value} = OTP.Bucket.get(bucket, "key")
     assert value == "value"
+  end
+
+  test "delete bucket by key", %{bucket: bucket} do
+    {:just, value} = OTP.Bucket.delete(bucket, "setup")
+    assert value == "init"
+    r = OTP.Bucket.get(bucket, "setup")
+    assert r == :nothing
   end
 end
