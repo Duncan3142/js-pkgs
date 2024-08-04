b699ca81b63aa50945036b572763fad99e5901c8
Duncan Giles <duncan3142@gmail.com>
2024-08-04 07:35:27 +0100

    command test setup

--- apps/hermes_umbrella/apps/hermes_server/test/hermes_server/command_test.exs
+++ apps/hermes_umbrella/apps/hermes_server/test/hermes_server/command_test.exs
@@ -1,4 +1,23 @@
 defmodule HermesServer.CommandTest do
+  @moduledoc """
+  HermesServer.Command tests
+  """
   use ExUnit.Case, async: true
   doctest HermesServer.Command
+
+  setup ctx do
+    sup_name = :"#{ctx.test}super"
+    reg_name = :"#{ctx.test}reg"
+    start_supervised!({DynamicSupervisor, [name: sup_name, strategy: :one_for_one]})
+    start_supervised!({Otp.Registry, [supervisor: sup_name, name: reg_name]})
+    HermesServer.Command.run(reg_name, {:create, "setup_bucket"})
+    HermesServer.Command.run(reg_name, {:put, "setup_bucket", "setup_key", "setup_value"})
+
+    %{reg_name: reg_name}
+  end
+
+  test "run get", %{reg_name: reg_name} do
+    assert HermesServer.Command.run(reg_name, {:get, "setup_bucket", "setup_key"}) ==
+             {:ok, "setup_value\nOK\n"}
+  end
 end
